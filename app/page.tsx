204ae19f52244a9b326cf04b6ff0816b12622635
Marcin Kuna <marcin.m.kuna@gmail.com>
2023-10-02 20:12:08 +0200

    typings

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
 import Image from "next/image";
+import BlogList from "./components/BlogList";
 
-export default function Home() {
+const query = groq`
+  *[_type=='post']{
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+export default async function Home() {
+  const posts = await client.fetch(query);
+
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <h1 className="text-4xl">Welcome to the blog!</h1>
     //   <p>Some content</p>
     // </main>
-    <div></div>
+    <div>
+      <BlogList posts={posts} />
+    </div>
   );
 }
