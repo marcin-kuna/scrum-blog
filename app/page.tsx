65dab079bdf56875382cbd6a41f615ea81db47ef
Marcin Kuna <marcin.m.kuna@gmail.com>
2023-10-02 19:02:59 +0200

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl">Welcome to the blog!</h1>
-      <p>Some content</p>
-    </main>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   <h1 className="text-4xl">Welcome to the blog!</h1>
+    //   <p>Some content</p>
+    // </main>
+    <div></div>
   );
 }
